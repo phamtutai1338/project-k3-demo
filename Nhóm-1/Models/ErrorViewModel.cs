a8d487895e0468eb844b5717cacdbfb89f9f3f83
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2023-01-03 14:44:05 +0700

    up date

---
+++ Nhóm-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nhóm_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
